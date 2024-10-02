b5e0121f9830db29f23bd9b5d2acd71f25579a5b
RaphaelSantos01 <raphael.afs06@gmail.com>
2024-10-02 18:19:53 -0300

    Validação de Campos - Feedback Customizados

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -37,13 +37,27 @@ class ContatoController extends Controller
     public function salvar(Request $request) {
         
         //Validar os campos antes de inserir
-        $request->validate([
-            'nome' => 'required|min:3|max:40|unique:contatos',
+        $regras = [
+            'nome' => 'required|min:3|max:40',
             'telefone' => 'required|min:9|max:20',
             'email' => 'email',
             'motivo_contato' => 'required',
             'mensagem' => 'required|max:2000'
-        ]);
+        ];
+
+        $feedback = [
+            'nome.required' => 'O campo nome precisa ser preenchido',
+            'nome.min' => 'O campo nome deve ter no mínimo 3 caracteres',
+            'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
+            'telefone.required' => 'O campo telefone deve ser preenchido',
+            'telefone.min' => 'O campo telefone deve ter no mínimo 9 caracteres',
+            'telefone.max' => 'O campo telofone deve ter no máximo 20 caracteres',
+            'email.email' => 'O campo email deve ser um email válido',
+            'motivo_contato.required' => 'O campo motivo de contato precisa ser preenchido',
+            'mensagem.max' => 'O campo mensagem precisa ter no máximo 2000 caracteres'
+        ];
+
+        $request->validate($regras, $feedback);
 
         //inserir os campos se for validado
         Contato::create($request->all());
